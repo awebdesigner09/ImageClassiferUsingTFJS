b1f3e33c9192c0eee88db3c69d9b4206580f5971
Naga Sai Katakamsetty <awebdesigner09@gmail.com>
2019-08-27 22:48:45 +0530

    Added Styles to final version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let net;
 
+const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
 
 async function setupWebcam(){
@@ -27,14 +28,42 @@ async function app() {
     console.log('Successfully loaded model');
 
     await setupWebcam();
-    while(true){
-        const result = await net.classify(webcamElement);
 
-        document.getElementById('console').innerText = `
-            predection: ${result[0].className}\n
-            probability: ${result[0].probability}
+    const addExample = classId => {
+        const activation = net.infer(webcamElement, 'conv_preds');
+        console.log('AddExample:'+classId);
+        classifier.addExample(activation, classId);
+        
+        const canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        context.drawImage(webcamElement, 0, 0, 224, 224);
+        var elem = document.createElement("img");
+        elem.setAttribute("src", canvas.toDataURL());
+        elem.setAttribute("height", "50");
+        elem.setAttribute("width", "50");      
+        document.getElementById("imgs-"+classId).appendChild(elem);
+    };
+
+    document.getElementById('class-a').addEventListener('click', () => addExample(0));
+    document.getElementById('class-b').addEventListener('click', () => addExample(1));
+    document.getElementById('class-c').addEventListener('click', () => addExample(2));
+
+    while(true){
+
+        if(classifier.getNumClasses() > 0){
+
+            const activation = net.infer(webcamElement, 'conv_preds');
+            const result = await classifier.predictClass(activation);
+
+            const classes = ['A', 'B', 'C'];
+
+            document.getElementById('console').innerText = `
+            Prediction: ${classes[result.classIndex]}\n
+            Probability: ${result.confidences[result.classIndex]}
         `;
 
+        }
+       
         await tf.nextFrame();
     }
 }
