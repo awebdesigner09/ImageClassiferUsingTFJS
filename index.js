873c524a00eaa0b664b30d21851f2bc89b9a116a
Naga Sai Katakamsetty <awebdesigner09@gmail.com>
2019-08-27 08:20:48 +0530

    Added webcamElement and script for classification for each frame

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 let net;
 
+const webcamElement = document.getElementById('webcam');
+
+async function setupWebcam(){
+    return new Promise((resolve,reject) => {
+        const navigatorAny = navigator;
+        navigator.getUserMedia = navigator.getUserMedia || navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia || navigatorAny.msGetUserMedia;
+
+        if(navigator.getUserMedia){
+            navigator.getUserMedia({video: true},
+                stream => {
+                    webcamElement.srcObject = stream;
+                    webcamElement.addEventListener('loadeddata', () => resolve(), false);
+                },
+                error => reject());
+        }else{
+            reject();
+        }
+    });
+}
+
 async function app() {
     console.log('Loading mobilenet..');
 
     net = await mobilenet.load();
     console.log('Successfully loaded model');
 
-    const imgEl = document.getElementById('img');
-    const result = await net.classify(imgEl);
-    console.log(result);
+    await setupWebcam();
+    while(true){
+        const result = await net.classify(webcamElement);
+
+        document.getElementById('console').innerText = `
+            predection: ${result[0].className}\n
+            probability: ${result[0].probability}
+        `;
+
+        await tf.nextFrame();
+    }
 }
 
 app();
